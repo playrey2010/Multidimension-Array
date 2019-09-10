6709246c35d44514224f07842253f4d7c8b7b3e2
playrey2010 <playrey2010@gmail.com>
2019-09-10 13:19:15 -0400

    second solution implements arrays

--- src/MultiDimArrayDemo.java
+++ src/MultiDimArrayDemo.java
@@ -10,13 +10,28 @@ public class MultiDimArrayDemo {
 //            System.out.println("");
 //
 //        }
-
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.print("row " + i + " col " + j + " ");
+//
+//        for (int i = 0; i < 10; i++) {
+//            for (int j = 0; j < 5; j++) {
+//                System.out.print("row " + i + " col " + j + " ");
+//            }
+//            System.out.println("");
+//        }
+        String [][] multTable = new String[12][12];
+        // this solution does not use arrays.
+//        for (int i = 1; i <=12; i++) {
+//            for (int j = 1; j<=12; j++) {
+//                System.out.print(i*j + "\t");
+//            }
+//            System.out.println("");
+//        }
+        //This solution does use arrays.
+        for (int i = 1; i <=12; i++) {
+            for (int j = 1; j<=12; j++) {
+                multTable[i-1][j-1] = i*j + "\t";
+                System.out.print(multTable[i-1][j-1] + "\t");
             }
             System.out.println("");
-
         }
 
     }
