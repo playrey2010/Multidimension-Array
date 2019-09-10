df651eeb1be5779ca03e0664f67a0622a819374c
playrey2010 <playrey2010@gmail.com>
2019-09-10 11:59:54 -0400

    instruction don't match, finished

---
+++ src/MultiDimArrayDemo.java
@@ -0,0 +1,23 @@
+public class MultiDimArrayDemo {
+    public static void main(String[] args) {
+
+        int [][] rowcol = new int[10][5];
+
+//        for (int i = 0; i < 10; i++) {
+//            for (int j = 0; j < 5; j++) {
+//                rowcol[i][j] = ;
+//            }
+//            System.out.println("");
+//
+//        }
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("row " + i + " col " + j + " ");
+            }
+            System.out.println("");
+
+        }
+
+    }
+}
